2666e7f99321f5077e2af6adfb76e9a12791bf62
TaToonz <peerapath.t@gmail.com>
2019-09-06 22:34:38 +0700

    change romanLetterInt from map[string]int to map[rune]int
    for remove unnecessary casting

--- roman.go
+++ roman.go
@@ -1,20 +1,20 @@
 package numeral
 
-var romanLetterInt = map[string]int{
-	"I": 1,
-	"V": 5,
-	"X": 10,
-	"L": 50,
-	"C": 100,
-	"D": 500,
-	"M": 1000,
+var romanLetterInt = map[rune]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
 }
 
 // RomanToInt converts roman numerals to integer
 func RomanToInt(input string) int {
 	result := 0
 	for _, letter := range input {
-		result += romanLetterInt[string(letter)]
+		result += romanLetterInt[letter]
 	}
 
 	return result
