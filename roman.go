175c34866dd2d2b1b27ac59b6e2aaff4579bf212
TaToonz <peerapath.t@gmail.com>
2019-09-06 22:17:59 +0700

    use map for storing each roman's letter values

--- roman.go
+++ roman.go
@@ -1,23 +1,16 @@
 package numeral
 
+var romanLetterInt = map[string]int{
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
+}
+
 // RomanToInt converts roman numerals to integer
 func RomanToInt(input string) int {
-	switch input {
-	case "I":
-		return 1
-	case "V":
-		return 5
-	case "X":
-		return 10
-	case "L":
-		return 50
-	case "C":
-		return 100
-	case "D":
-		return 500
-	case "M":
-		return 1000
-	}
-
-	return 0
+	return romanLetterInt[input]
 }
